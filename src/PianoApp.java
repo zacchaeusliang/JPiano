7a726b1719ff58050b52ffa73d894dc6083db7c0
zacchaeus <z.liang111@gmail.com>
2017-05-08 14:00:58 -0400

    Initial commit

---
+++ src/PianoApp.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by account2 on 2017-04-08.
+ */
+
+public class PianoApp extends Application {
+    public void start(Stage primaryStage) {
+        PianoView pView = new PianoView();
+        primaryStage.setTitle("Piano App");
+        primaryStage.setResizable(true);
+        primaryStage.setScene(new Scene(pView, 285, 205));
+        primaryStage.show();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ src/PianoView.java
@@ -0,0 +1,59 @@
+/**
+ * Created by account2 on 2017-04-08.
+ */
+
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import org.jfugue.player.Player;
+
+import javafx.event.ActionEvent;
+
+public class PianoView extends Pane {
+
+
+    public PianoView() {
+        GridPane aPane = new GridPane();
+        Player player = new Player();
+        
+        aPane.setPadding(new Insets(10, 10, 10, 10));
+        aPane.setHgap(1);
+        aPane.setVgap(1);
+        String [] notes={"A4","B4b","B5","D5b","D5","Eb","E5","F5","G5b","A6"};
+
+        for (int row = 1; row < 10; row++) {
+            Button b = new Button();
+            b.setPrefWidth(50);
+            b.setPrefHeight(50);
+
+            b.setStyle("-fx-base: WHITE;");
+           b.setText(notes[row]);
+           b.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    player.play(b.getText());
+                }
+            });
+
+
+            aPane.add(b,  row,0);
+
+
+
+
+        }
+        getChildren().add(aPane);
+        update();
+    }
+
+    public void update() {
+
+    }
+}
